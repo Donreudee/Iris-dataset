28517e2d99e8e62151eead8fc6e34732b3921187
Jelin <6531501042@lamduan.mfu.ac.th>
2025-04-22 15:29:08 +0700

    Add files via upload

--- App.py
+++ App.py
@@ -60,7 +60,11 @@ def load_data():
             'sepal_width': [3.5, 3.0, 3.2, 3.1, 3.6, 3.9, 3.4, 3.4, 2.9, 3.1],
             'petal_length': [1.4, 1.4, 1.3, 1.5, 1.4, 1.7, 1.4, 1.5, 1.4, 1.5],
             'petal_width': [0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1],
-            'species': ['setosa'] * 10
+            'species': [
+                'setosa', 'setosa', 'setosa',
+                'versicolor', 'versicolor', 'versicolor',
+                'virginica', 'virginica', 'virginica', 'virginica'
+            ]
         }
         df = pd.DataFrame(data)
     return df
